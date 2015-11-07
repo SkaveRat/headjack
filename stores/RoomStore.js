0e6f20a98451f50c06353a9ba8e65e6884cb5538
SkaveRat <skaverat@funlynx.de>
2015-11-07 02:05:22 +0100

    resetting localstorage for room list

--- stores/RoomStore.js
+++ stores/RoomStore.js
@@ -3,28 +3,14 @@ import Reflux from 'reflux';
 import MatrixActions from '../actions/MatrixActions.js';
 
 export default Reflux.createStore({
-    get: function() {
-        return this.data;
-    },
-    set: function (rooms) {
-        this.data = rooms;
-        localStorage.rooms = JSON.stringify(rooms);
-    },
-
-    getInitialState: function() {
-        if(localStorage.rooms) {
-            return JSON.parse(localStorage.rooms);
-        }
-
-        return {};
-    },
+    data: {},
 
     init: function () {
         this.listenTo(MatrixActions.init.success, this.handleInitSync)
     },
 
     handleInitSync: function (data) {
-        this.set(data.rooms);
-        this.trigger(this.get());
+        this.data.rooms = data.rooms;
+        this.trigger(this.data.rooms);
     }
 });
